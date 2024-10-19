4efea528d4792a5f89dad002936bc91e0602d651
Phil MacEachron <phil@repl.it>
2024-10-19 13:28:52 -0700

    Add auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,15 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <div className={GeistSans.className}>
-      <Component {...pageProps} />
-    </div>
+    <ClerkProvider {...pageProps}>
+      <div className={GeistSans.className}>
+        <Component {...pageProps} />
+      </div>
+    </ClerkProvider>
   );
 };
 
